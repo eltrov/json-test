2d34580f4f231c5dc78e7fa268f3a71432fcb4b8
Chris <eltrov@gmail.com>
2020-03-27 23:00:58 -0500

    first full draft (ish)

--- acnh/script.js
+++ acnh/script.js
@@ -49,10 +49,41 @@ function populateHeader(jsonObj) {
       //if (j == 5) { }
       //if (j == 6) { th.colSpan = 1; th.bgColor = "pink"; }
 
-        var th = document.createElement("th"); // TABLE HEADER.        
-        th.innerHTML = hrs[i][col[j]];
+      
+
+      var th = document.createElement("th"); // TABLE HEADER.        
+      th.innerHTML = hrs[i][col[j]];
+
+      if (j == 5) { 
+      th.colSpan = 2; 
+      th.bgColor = "pink"; 
+      th.innerHTML = "Hours Available";
+      th.width = 300;
+      }
+
+      
+      if (j == 11) { 
+        th.colSpan = 2; 
+        th.bgColor = "pink"; 
+        th.innerHTML = "Months Available (Northern Hemisphere)";
+        th.width = 300;
+        }
+
+        if (j == 16) { 
+          th.colSpan = 2; 
+          th.bgColor = "pink"; 
+          th.innerHTML = "Months Available (Southern Hemisphere)";
+          th.width = 300;
+          }
+
+      if (j == 6 || j == 7) { }
+      else if (j == 8 || j == 9) { }
+      else if (j == 10) { }
+      else if (j == 12 || j == 13 || j == 14 || j == 15) { }
+      else if (j == 17 || j == 18 || j == 19 || j == 20) { }
+      else { tr.appendChild(th); }
+
         
-        if (j != 5) {tr.appendChild(th);}
 
       /*
       var tabCell = tr.insertCell(-1);
@@ -92,17 +123,83 @@ function populateHeader(jsonObj) {
         tabCell.appendChild(img);
       } 
       else if (j == 5) {
-        var AllDay = heroes[i][col2[j]];
-        if (AllDay === true) {
+        var TimeLimited = heroes[i][col2[j]];
+        if (TimeLimited === false) {
           tabCell.innerHTML = "All day";
+          tabCell.colSpan = 2;
          }
-       }      
-        else {
-        tabCell.innerHTML = heroes[i][col2[j]];
-      }
+         else {
+           timeStart = heroes[i][col2[j+1]]
+           timeEnd = heroes[i][col2[j+2]]
+           
+           time2Start = heroes[i][col2[8]]
+           time2End = heroes[i][col2[9]]
 
-    }
+          tabCell.colSpan = 2;
+          if (time2Start == "") {
+            tabCell.innerHTML = timeStart + " - " + timeEnd;
+          }
+          else {
+            tabCell.innerHTML = timeStart + " - " + timeEnd + ", " + time2Start + " - " + time2End;
+          }
+          }
+       }     
+       else if (j == 11) {
+        var TimeLimited = heroes[i][col2[j]];
+        var timeStart = heroes[i][col2[12]];
+        var timeEnd = heroes[i][col2[13]];
+
+        if (TimeLimited === false) {
+          tabCell.innerHTML = "All year";
+          tabCell.colSpan = 2;
+         }
+
+         // Logic for Salmon/King Salmon, only available in September
+         else if (timeStart == timeEnd)
+         {
+          tabCell.innerHTML = timeStart;
+          tabCell.colSpan = 2;
+         }
+
+        else {
+          timeStart = heroes[i][col2[j+1]]
+          timeEnd = heroes[i][col2[j+2]]
+          tabCell.colSpan = 2;
+          tabCell.innerHTML = timeStart + " - " + timeEnd;
+         }
+       }           
+
+       else if (j == 16) {
+        var TimeLimited = heroes[i][col2[11]];
+        var timeStart = heroes[i][col2[16]];
+        var timeEnd = heroes[i][col2[17]];
+
+        if (TimeLimited === false) {
+          tabCell.innerHTML = "All year";
+          tabCell.colSpan = 2;
+         }
+
+         // Logic for Salmon/King Salmon, only available in September
+         else if (timeStart == timeEnd)
+         {
+          tabCell.innerHTML = timeStart;
+          tabCell.colSpan = 2;
+         }
+
+        else {
+          tabCell.colSpan = 2;
+          tabCell.innerHTML = timeStart + " - " + timeEnd;
+         }
+       }  
+
+       else if (j == 6 || j == 7 || j == 8 || j == 9) { tabCell.innerHTML = ""; tabCell.style = "display:none;"}
+       else if (j == 10) { tabCell.innerHTML = ""; tabCell.style = "display:none;"}
+       else if (j == 12 || j == 13 || j == 14 || j == 15) { tabCell.innerHTML = ""; tabCell.style = "display:none;"}
+       else if (j == 17 || j == 18 || j == 19 || j == 20) { tabCell.innerHTML = ""; tabCell.style = "display:none;"}
+
+       else { tabCell.innerHTML = heroes[i][col2[j]]; }
   }
+}
 
   // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
   var elem = document.createElement('div');
